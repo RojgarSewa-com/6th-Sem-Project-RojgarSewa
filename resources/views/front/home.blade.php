b58acb2cf579ef0b34b980a9d1809b0a2fe296df
Sharad-yadav <sharad08101@gmail.com>
2022-07-17 15:24:52 +0545

    These are the changes i made in our project

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,17 +1,144 @@
 @extends('front.layouts.app')
 @section('content')
 <style>
+    *{
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+}
     .box_nav_bar {
     height: 10em;
 }
-.row.justify-content-md-center {
-    height: 10em;
+ 
+
+.containerss{
+    
+    display: flex;
+    margin-left:63px;
+    padding: 10px 10px 10px 10px;
+    flex-wrap: wrap;
+    flex-direction : row;
+    
 }
 
-.col-md-auto {
-    text-align: center;
-    margin-top: 4em;
+.card {
+    margin: 8px;
+    cursor: pointer;
+    color: white;
 }
+.card{
+    
+    background-color: rgb(94, 58, 128);
+    width:200px;
+    height:239px;
+    margin:20px;
+    text-align: center;
+    border-radius: 20px;
+    
+}
+.icon{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/elec.jpg");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon1{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/teacher.png");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon2{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/web.jpg");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon3{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/plumber.png");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon4{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/laptop-repair-3690681-3073968.png");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon5{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/Tech support.jpg");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon6{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/salesman (1).jpg");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon7{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/plumber.png");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon8{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/IT technician.jpg");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.icon9{
+    
+    height:200px;
+    background-image:url("http://127.0.0.1:8000/images/HomeTutor.png");
+    background-size: cover;
+    border-radius: 15px 15px 0 0;
+
+}
+.card:hover{
+    background-color: lightcoral;
+    color: wheat;
+    transform: scale(1.2);
+    transition: all 0.5s ease;
+
+}
+#teext{
+    font-size:20px;
+}
+.containers {
+  display:flex;
+  justify-content:center;
+  color:black;
+  margin-top:2px;
+}
+.containers:hover{
+    background-color: lightcoral;
+    color: wheat;
+    transform: scale(1.2);
+    transition: all 0.5s ease;
+}
+
  </style>
 <div class="container-fluid banner text-center">
 <h1 style="padding-top: 6rem;">Services On Your Finger.</h1>
@@ -24,33 +151,97 @@
 </div>
 {{-- Footer navigation bar start --}}
 
-<div class="container" style="height: 160px; background-color: white">
-    <div class="box_nav_bar">
-        <div class="row justify-content-md-center">
-          <div class="col-md-auto">
-            Top Services
-          </div>
-          <div class="col-md-auto">
-            <a href="#"><i class="fab fa-facebook"></i></a>
-          </div>
-          <div class="col-md-auto">
-            Variable width content
-          </div>
-          <div class="col-md-auto">
-            Variable width content
-          </div>
-          <div class="col-md-auto">
-            Variable width content
-          </div>
-          <div class="col-md-auto">
-            Variable width content
-          </div>
-          <div class="col-md-auto">
-            3 of 3
-          </div>
-        </div>
-    </div>
+<!-- TOP SERVICES PART START -->
+
+<div class="containers">
+<h1>Top Services</h1>
 </div>
+
+<div class="containerss">
+
+        <div class="card">
+            <div class="icon">
+            </div>
+            <h1 id="teext">Electrician</h1>
+            
+        </div>
+
+        <div class="card">
+            <div class="icon1">
+            </div>
+            <h1 id="teext">Teacher</h1>
+        </div>
+
+        <div class="card">
+            <div class="icon2">
+            </div>
+            <h1 id="teext">Web Designer</h1>
+        </div>
+
+        <div class="card">
+            <div class="icon3">
+            </div>
+            <h1 id="teext">Plumber</h1>
+        </div>
+       <div class="card">
+            <div class="icon4">
+            </div>
+            <h1 id="teext">Laptop Repair</h1>
+        </div>
+         
+        <div class="card">
+            <div class="icon5">
+            </div>
+            <h1 id="teext">Tech-Support</h1>
+        </div>
+                <div class="card">
+            <div class="icon6">
+            </div>
+            <h1 id="teext">SalesMan</h1>
+        </div>
+                <div class="card">
+            <div class="icon7">
+            </div>
+            <h1 id="teext">Photographer</h1>
+        </div>
+
+         <div class="card">
+            <div class="icon8">
+            </div>
+            <h1 id="teext">IT Technician</h1>
+            
+        </div>
+
+         <div class="card">
+            <div class="icon9">
+            </div>
+            <h1 id="teext">Home Tutor</h1>
+            
+        </div>
+
+    </div>
+
+
+</div>
+
+<!-- TOP SERVICES PART END -->
+
+<!-- OTHER SERVICES PART START -->
+
+<div class="containers">
+<h1>Other Services</h1>
+</div>
+
+
+
+
+
+
+
+
+
+
+    
 
 {{-- Footer navigation bar End --}}
 
