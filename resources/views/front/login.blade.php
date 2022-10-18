c1d74cf9c3152977f8bca0db9e3d590893136429
Sagar <luitelsagar7@gmail.com>
2022-10-18 14:58:48 +0545

    login bar changes

--- resources/views/front/login.blade.php
+++ resources/views/front/login.blade.php
@@ -1,15 +1,15 @@
 @extends('front.layouts.app')
 @section('content')
     <div class="container-fluid login">
-        <div class="row d-flex justify-content-center">
-            <div class="col-6 p-5 border" style="background-color: white">
-                <div class="row">
+        <!-- <div class="row d-flex justify-content-center"> -->
+            <div class="row" style="background-color: white">
+                <div class="col-sm-6">
                     <a href="{{route('user-login')}}"><button type="button" class="btn btn-primary btn-lg btn-block" style="width: 100%">Login as User</button></a>
                 </div>
-                <div class="row mt-5">
+                <div class="col-sm-6">
                     <a href="{{route('provider-login')}}"><button type="button" class="btn btn-success btn-lg btn-block" style="width: 100%">Login as Provider</button></a>
                 </div>
             </div>
-        </div>
+        <!-- </div> -->
     </div>
 @endsection
--- resources/views/front/register.blade.php
+++ resources/views/front/register.blade.php
@@ -1,16 +1,16 @@
 @extends('front.layouts.app')
 @section('content')
     <div class="container-fluid login">
-        <div class="row d-flex justify-content-center">
-            <div class="col-6 p-5 border" style="background-color: white">
-                <div class="row">
+        <!-- <div class="row d-flex justify-content-center"> -->
+            <div class="row" style="background-color: white">
+                <div class="col-sm-6">
                     <a href="{{route('user-register')}}"><button type="button" class="btn btn-primary btn-lg btn-block" style="width: 100%">Sign Up as User</button></a>
                 </div>
-                <div class="row mt-5">
+                <div class="col-sm-6">
                     <a href="{{route('provider-register')}}"><button type="button" class="btn btn-success btn-lg btn-block" style="width: 100%">Sign Up as Provider</button></a>
                 </div>
             </div>
-        </div>
+        <!-- </div> -->
     </div>
 
 @endsection
